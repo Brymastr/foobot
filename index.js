b8a5a5a79fc80a96bb36f8534c94a34f18bf8e12
Brycen Dorsay <bdorsay@rbauction.com>
2016-05-04 17:03:11 -0700

    Comments

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
+// Require libraries
 var mongoose = require('mongoose');
 var express = require('express');
 var http = require('http');
 var request = require('request');
 var bodyParser = require('body-parser');
 
+// App setup
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+// Configurations
 const token = process.env.FOOBOT_TOKEN || '223951341:AAGJXSvta7MkEEoeM3Fy3FCMjn_8ho3YV10';
-const db = '//TODO: db connection';
+const db = process.env.FOOBOT_DB_CONN || '//TODO: db connection';
 const telegram = 'https://api.telegram.org/bot' + token;
 const url = '//TODO: url of this server for the webhook to be sent to';
 const port = process.env.FOOBOT_PORT || 9000;
 
-// mongoose.connect(db);
-// mongoose.connection.on('open', function() {
-//   console.log('Mongo connection is open. Connected to: ' + db);
-// });
 
-// request.post(telegram + '/setWebhook', {
-//   json: {
-//     url: url,
-//     certficate: '//TODO: generate self-signed cert'
-//   }
-// }, function(err, response, body) {
-//   if(err) console.log(err);
-// });
-
+// Function to get all updates from telegram bot api
 var getUpdates = function(done) {
   var result = [];
   
@@ -47,6 +38,7 @@ var getUpdates = function(done) {
   });
 }
 
+// Routes
 app.post('/foobot/webhook/:token', function(req, res, next) {
   res.send('foo');
 });
@@ -57,7 +49,22 @@ app.get('/foobot/updates', function(req, res) {
   });
 });
 
+// Start listener
 http.createServer(app).listen(port, function() {
   console.log("server listening on port " + port);
 });
 
+
+// mongoose.connect(db);
+// mongoose.connection.on('open', function() {
+//   console.log('Mongo connection is open. Connected to: ' + db);
+// });
+
+// request.post(telegram + '/setWebhook', {
+//   json: {
+//     url: url,
+//     certficate: '//TODO: generate self-signed cert'
+//   }
+// }, function(err, response, body) {
+//   if(err) console.log(err);
+// });
