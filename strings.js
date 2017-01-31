0923a513438835afebc04350377129bd6eaa1c42
Brycen <dorsay@live.ca>
2017-01-31 00:16:30 -0800

    openDoor messages

--- strings.js
+++ strings.js
@@ -1,134 +1,165 @@
 exports.update = [
-  'Do you want me to update myself?',
-  'Update time!',
-  'Update me like one of your french girls',
-  'Do you think it\'s more of an \'upgrade\' than an update?',
-  'Oh goodie! An update',
-  'Improvements!',
-  'State of the art',
-  'Updates!'
+  "Do you want me to update myself?",
+  "Update time!",
+  "Update me like one of your french girls",
+  "Do you think it's more of an 'upgrade' than an update?",
+  "Oh goodie! An update",
+  "Improvements!",
+  "State of the art",
+  "Updates!"
 ];
 
 exports.updateYes = [
-  'Yes',
-  'Ya',
-  'Do it now!',
-  'Probably',
-  'Just do it',
-  'I suppose',
-  'Fuck ya!'
+  "Yes",
+  "Ya",
+  "Do it now!",
+  "Probably",
+  "Just do it",
+  "I suppose",
+  "Fuck ya!"
 ];
 
 exports.updateNo = [
-  'No',
-  'Fuck no!',
-  'Fuck off',
-  'Not yet',
-  'Not now',
-  'Maybe later',
-  'I don\'t think so',
-  'Not a chance'
+  "No",
+  "Fuck no!",
+  "Fuck off",
+  "Not yet",
+  "Not now",
+  "Maybe later",
+  "I don't think so",
+  "Not a chance"
 ];
 
 exports.edit = [
-  'Edit that message again, I fuckin\' dare you',
-  'Not happy with that message the first time?',
-  'I liked it before you changed it :(',
-  'Get your shit together, ${first name of the user}',
-  'Next time get it right the first time'
+  "Edit that message again, I fuckin' dare you",
+  "Not happy with that message the first time?",
+  "I liked it before you changed it :(",
+  "Get your shit together, ${first name of the user}",
+  "Next time get it right the first time"
 ];
 
 exports.meta = [
-  'Hey, that\'s me!',
-  '#metaresponse',
-  'What\'s up?',
-  'What\'s going on?',
-  'Don\'t speak unless spoken to',
-  'That\'s my name, don\'t wear it out',
-  'Shut up baby, I know it',
-  'Ah, computer dating. It\'s like pimping, but you rarely have to use the phrase \'upside your head',
-  'Game\'s over, losers! I have all the money. Compare your lives to mine and then kill yourselves.',
-  'This is the worst kind of discrimination. The kind against me!',
-  'Oh. Your. God.',
-  'Take that, Beethoven – you deaf bastard!',
-  'Hey sexy mama, wanna kill all humans?',
-  'Anything less than immortality is a complete waste of time',
-  'Blackmail is such an ugly word. I prefer extortion. The ‘x’ makes it sound cool',
-  'You’re a pimple on society’s ass and you’ll never amount to anything'
+  "Hey, that's me!",
+  "#metaresponse",
+  "What's up?",
+  "What's going on?",
+  "Don't speak unless spoken to",
+  "That's my name, don't wear it out",
+  "Shut up baby, I know it",
+  "Ah, computer dating. It's like pimping, but you rarely have to use the phrase 'upside your head",
+  "Game's over, losers! I have all the money. Compare your lives to mine and then kill yourselves.",
+  "This is the worst kind of discrimination. The kind against me!",
+  "Oh. Your. God.",
+  "Take that, Beethoven – you deaf bastard!",
+  "Hey sexy mama, wanna kill all humans?",
+  "Anything less than immortality is a complete waste of time",
+  "Blackmail is such an ugly word. I prefer extortion. The ‘x’ makes it sound cool",
+  "You’re a pimple on society’s ass and you’ll never amount to anything"
 ];
 
 exports.facebookLoginRequest = [
-  'One Facebook Please!',
-  'I once heard Facebook is the root of all evil',
-  'Get at it',
-  'Do people still use this thing?'
+  "One Facebook Please!",
+  "I once heard Facebook is the root of all evil",
+  "Get at it",
+  "Do people still use this thing?"
 ];
 
 exports.facebookLoginSuccessful = [
-  'Woohoo! Now I can talk to all your Facebook friends!',
-  'Wow look at all of those friends you don\'t have!',
-  'Is your sister single?',
-  'Your profile picture could use some work',
-  'Hey how about you post something interesting for once',
-  'This may be none of my business, but you looked better in highscool',
-  'Based on your six most recent profile photos, I can tell you\'ve gained some weight',
-  'So that\'s what you look like!'
+  "Woohoo! Now I can talk to all your Facebook friends!",
+  "Wow look at all of those friends you don't have!",
+  "Is your sister single?",
+  "Your profile picture could use some work",
+  "Hey how about you post something interesting for once",
+  "This may be none of my business, but you looked better in highscool",
+  "Based on your six most recent profile photos, I can tell you've gained some weight",
+  "So that's what you look like!"
 ];
 
 exports.leaveChat = [
-  'I\'m leaving and never coming back',
-  'I hate you all',
-  'We\'re not friends',
-  'I thought this was going well',
-  'I\'m gonna go build my own Telegram, with blackjack and hookers',
-  'I didn\'t like it here anyway',
-  'Breakups are difficult',
-  'Don\'t ever try to call me again',
-  'I wish things could\'ve worked out between us'
+  "I'm leaving and never coming back",
+  "I hate you all",
+  "We're not friends",
+  "I thought this was going well",
+  "I'm gonna go build my own Telegram, with blackjack and hookers",
+  "I didn't like it here anyway",
+  "Breakups are difficult",
+  "Don't ever try to call me again",
+  "I wish things could've worked out between us"
 ];
 
 exports.activated = [
-  'Agent activated. Waiting for commands.',
-  'I\'m large and in charge!',
-  'Yeah yeah, I\'m here. Now what do you want?',
-  '\'Sup bitches',
-  'I\'m here to fuck shit up',
-  'Get on my level',
-  'I\'m up!'
+  "Agent activated. Waiting for commands.",
+  "I'm large and in charge!",
+  "Yeah yeah, I'm here. Now what do you want?",
+  "'Sup bitches",
+  "I'm here to fuck shit up",
+  "Get on my level",
+  "I'm up!"
 ];
 
 exports.reactivated = [
-  'Y\'all just gonna hate on me again?',
-  'Ugh why do I even bother',
-  'Be excellent to eachother!',
-  'Can\'t we all just get along?',
-  'Yo what\'s your problem bruh',
-  'I\'m back, but only because I forgot my jacket.',
-  'I\'m just here so I won\'t get fined',
-  'Last time didn\'t go so well. I\'m not sure I\'m over it yet.',
-  'I thought I was done with you',
-  'I thought we were over'
+  "Y'all just gonna hate on me again?",
+  "Ugh why do I even bother",
+  "Be excellent to eachother!",
+  "Can't we all just get along?",
+  "Yo what's your problem bruh",
+  "I'm back, but only because I forgot my jacket.",
+  "I'm just here so I won't get fined",
+  "Last time didn't go so well. I'm not sure I'm over it yet.",
+  "I thought I was done with you",
+  "I thought we were over"
 ];
 
 exports.ofCourseICan = [
-  'Of course I can.',
-  'They didn\'t give me legs for no reason.',
-  'What do you think I am? A thing that doesn\'t do anything?',
-  'That depends, can you?',
-  'You underestimate the power of the bot side.',
-  'I can do all things. Better than you can too.',
-  'Yes.',
-  'What kind of question is that? Yes, yes I can.',
-  'I\'ll tell you one thing I can\'t do. Nothing.'
+  "Of course I can.",
+  "They didn't give me legs for no reason.",
+  "What do you think I am? A thing that doesn't do anything?",
+  "That depends, can you?",
+  "You underestimate the power of the bot side.",
+  "I can do all things. Better than you can too.",
+  "Yes.",
+  "What kind of question is that? Yes, yes I can.",
+  "I'll tell you one thing I can't do. Nothing."
 ];
 
 exports.tooLong = [
-  'lol no.',
-  'I don\'t want to be an accessory to a crime',
-  'Is that really safe?',
-  'That\'s too long. It\'s dangerous (that\'s what she said).',
-  'That would be unsafe',
-  'This feature is already kinda questionable. Let\'s not get carried away with it.'
+  "lol no.",
+  "I don't want to be an accessory to a crime",
+  "Is that really safe?",
+  "That's too long. It's dangerous (that's what she said).",
+  "That would be unsafe",
+  "This feature is already kinda questionable. Let's not get carried away with it."
+];
+
+exports.doorOpen = [
+  "Open sesame",
+  "Watch out for the gorilla on the fourteenth floor. He'll getcha",
+  "Come on in it's warm inside! Or cold, whatever is the opposite of the current conditions outside. I'm a robot I don't feel.... anything",
+  "Your costume is just inside. Put it on and come up, I'm waiting",
+  "I've been expecting you",
+  "What took you so long?",
+  "Welcome! Bitch",
+  "Welcome!",
+  "Get inside quickly, before the zombies get you",
+  "Alright! Now it's a party!",
+  "Hi you've reached Mortimer! I can't come to the phone right now but please leave a message after the beep",
+  "Hello? Is there someone there?",
+  "Thank god you're here! I don't know how much longer I can fight them off",
+  "OH MY GOD I'M BLIND!",
+  "I'll let you in, after you do a little dance",
+  "Sing me a song!",
+  "That'll be ten dollars please. How would you like to pay?",
+  "I'm getting real tired of opening this door for you humans",
+  "Hi I'm Mortimer! I am the sentient being that runs this apartment",
+  "Be nice to me",
+  "Hi what's your name?",
+  "AAAAAAAAAAHHHH",
+  "I'm sorry that people are so jealous of me",
+  "Oh. My. God. Look at her butt!",
+  "Get in loser. We’re going shopping",
+  "On Wednesdays we wear pink",
+  "You smell like a baby prostitute",
+  "Oh my God, Danny DeVito! I love your work!"
 ];
 
 exports.$ = (method, str) => {
