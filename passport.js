799cc143520f55fa5177502cf6c7ad661e8e15da
Brycen <dorsay@live.ca>
2017-03-07 16:58:57 -0800

    log to see if passport is getting a user

--- passport.js
+++ passport.js
@@ -15,6 +15,8 @@ module.exports = passport => {
     }, (req, accessToken, refreshToken, profile, done) => {
       let params = JSON.parse(decodeURIComponent(req.query.state));
       usersController.getUser(params.user_id).then(user => {
+        console.log('FacebookStrategy')
+        console.log(user, profile)
         if(user) {
           user.facebook_id = profile.id;
           user.facebook_token = accessToken;
